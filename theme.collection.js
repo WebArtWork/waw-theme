e928e8ed287e4fd303bf8e0bcacddf59b0b4c95c
Nika <129855721+nikiquickie@users.noreply.github.com>
2023-12-26 14:52:10 +0200

    Update theme.collection.js

--- theme.collection.js
+++ theme.collection.js
@@ -1,4 +1,3 @@
-
 module.exports = async function (waw) {
 	const Schema = waw.mongoose.Schema({
 		folder: String,
@@ -6,14 +5,20 @@ module.exports = async function (waw) {
 		description: String,
 		thumb: String,
 		variables: {},
-		variablesInfo: [{
-			variable: String,
-			description: String,
-			thumb: String
-		}],
-		author: { type: waw.mongoose.Schema.Types.ObjectId, ref: 'User' },
-		url: { type: String, unique: true, sparse: true, trim: true }
+		variablesInfo: [
+			{
+				variable: String,
+				description: String,
+				thumb: String,
+			},
+		],
+		module: {
+			type: String,
+			enum: ["operator", "store"],
+		},
+		author: { type: waw.mongoose.Schema.Types.ObjectId, ref: "User" },
+		url: { type: String, unique: true, sparse: true, trim: true },
 	});
 
-	return waw.Theme = waw.mongoose.model('Theme', Schema);
-}
+	return (waw.Theme = waw.mongoose.model("Theme", Schema));
+};
